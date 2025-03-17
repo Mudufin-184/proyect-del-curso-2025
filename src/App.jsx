a298546885e1610bfc8a6648b6e2a7985674dd2d
Mudufin-184 <velandiafonseca@gmail.com>
2025-03-17 12:36:45 -0500

    Se creo la lista de cripto monedas y se agrega asios, tambien se creo hoja de estilos app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import './App.css'
 function App() {
+  const [criptos, setCriptos] = useState([]); 
+  const apiUrl = "https://api.coincap.io/v2/"; 
+
+  useEffect(() => {
+    axios.get(`${apiUrl}assets`)
+      .then((data) => {
+        setCriptos(data.data.data);
+      })
+      .catch((error) => {
+        console.error("La petición falló:", error);
+      });
+  }, []);  
+
+  if (criptos.length === 0) return <span>Cargando...</span>;
 
   return (
     <>
-    <h1>Hola mundo</h1>
+      <h1>Lista de Criptomonedas</h1>
+      <ol>
+        {criptos.map(({ id, name, priceUsd }) => (
+          <li key={id}>Nombre: {name} | Precio: ${parseFloat(priceUsd).toFixed(2)}</li>
+        ))}
+      </ol>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
