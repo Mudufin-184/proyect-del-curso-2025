48c2fe05b442165b84e29366807b55a076a3a708
Mudufin-184 <velandiafonseca@gmail.com>
2025-03-19 10:30:45 -0500

    Se mejora el hipervinculo de cripto por cripto moneda

--- src/components/cripto/Cripto.jsx
+++ src/components/cripto/Cripto.jsx
@@ -1,27 +1,23 @@
 import { Link } from "react-router-dom"
 import "./Cripto.css"
-function Cripto ({name, price, symbol,changePercent24Hr,id}) {
-    return(
-        <>
-        <Link to={`/criptomonedas/${id}`}>
-            <div className="cripto">
-                <h2>{name}</h2>
-                <div className="info">
-                <p><span className="label">Precio:</span> {Number(price).toFixed(4)} USD</p>
-                <p><span className="label">Simbolo:</span> {symbol}</p>
+
+function  Cripto({ id, name, priceUSD, symbol, changePercent24Hr }) {
+  return (
+    <div className="cripto">
+        <Link to={`/criptomonedas/${id}`}><h2>{name}</h2>
+            <div className="info">
+                <p><span className="label">Precio: </span>{parseFloat(priceUSD).toFixed(4)}</p>
+                <p><span className="label">Código: </span>{symbol}</p>
                 <p>
-                    <span className="label">Variación 24Hrs:</span> 
-                    <span className={parseFloat(changePercent24Hr) >  0 ? "positivo": "negativo"}>
-                        {Number(changePercent24Hr).toFixed(4)}%
-                    </span>
+                <span className="label">Variación 24hrs: </span>
+                <span className={ parseFloat(changePercent24Hr) > 0 ? "positivo" : "negativo" }>
+                    {parseFloat(changePercent24Hr).toFixed(3)}%
+                </span>
                 </p>
-                
-                </div>
             </div>
-        </Link>
-        </>
-        
-    )
+        </Link>  
+    </div>
+  )
 }
 
 export default Cripto
